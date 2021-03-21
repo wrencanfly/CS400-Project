0deed800eae56fe2fd9bf1cfbcab59662310eee5
Cai92 <79020263+Cai92@users.noreply.github.com>
2021-03-21 20:33:25 +0800

    Update insert and size

--- Backend.java
+++ Backend.java
@@ -1,14 +1,3 @@
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.NoSuchElementException;
-import java.util.zip.DataFormatException;
-
-
-
 //--== CS400 File Header Information ==--
 //Name: Yuanqing Cai
 //Email: cai92@wisc.edu
@@ -17,12 +6,27 @@ import java.util.zip.DataFormatException;
 //TA: Mu
 //Lecturer: Florian
 //Notes to Grader: <optional extra notes>
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.zip.DataFormatException;
+/**
+ * The Class Backend implements BackendInterface with several methods to modify the data.
+ * @author Rory Cai
+ *
+ */
 public class Backend implements BackendInterface{
-	
 	RedBlackTree<BankDataInterface> tree;
+	//The RedBlackTree stored the BankData
 	HashTableMap<String,Integer> map;
-	
-	
+	//The HashTableMap store phone as key and id as value
+	/**
+	 * method to put all data in csv into HashTableMap with phone as key and id as value
+	 * @param banklist
+	 */
 	private void putDataIntoHashTable(List<BankDataInterface> banklist) {
 		for(int i=0;i<banklist.size();i++) {
 			String key = banklist.get(i).getPhoneNum();
@@ -31,24 +35,32 @@ public class Backend implements BackendInterface{
 				map.put(key, value);
 		}
 	}
-	
+	/**
+	 * method to put all data in csv into RedBlackTree
+	 * @param banklist
+	 */
 	private void putDataIntoRedBlackTree(List<BankDataInterface> banklist) {
 		for(int i=0;i<banklist.size();i++) {
 			if(!tree.contains(banklist.get(i)))
 				tree.insert(banklist.get(i));
 		}
 	}
-	
-	
-	
+	/**
+	 * The constructor with File Reader which read the data in the given file and insert the data into RedBlackTree and HashTableMap
+	 * Will catch the exception throw by dataReader.
+	 * @param inputFileReader
+	 * @throws FileNotFoundException
+	 * @throws IOException
+	 * @throws DataFormatException
+	 */
 	public Backend(Reader inputFileReader) throws FileNotFoundException,IOException, DataFormatException{
 		try {
 			BankDataReader bank = new BankDataReader();
 			List<BankDataInterface> banklist = bank.readDataSet(inputFileReader);
 			tree = new RedBlackTree<BankDataInterface>();
 			map = new HashTableMap<String, Integer>();
-			putDataIntoRedBlackTree(banklist);
-			putDataIntoHashTable(banklist);
+			putDataIntoRedBlackTree(banklist);//put data into RedBlackTree
+			putDataIntoHashTable(banklist);//put data into HashTable
 		}catch(FileNotFoundException e) {
 			throw new FileNotFoundException();
 		}catch(IOException e) {
@@ -57,12 +69,22 @@ public class Backend implements BackendInterface{
 			throw new DataFormatException();
 		}
 	}
-	
+	/**
+	 * None argument constructor with no data in the RedBlackTree and HashTable
+	 */
 	public Backend() {
 		tree = new RedBlackTree<BankDataInterface>();
 		map = new HashTableMap<String, Integer>();
 	}
-	
+	/**
+	 * GetMember method which return the data of the member with given phone.
+	 * Use the given phone to get the id from the HashTableMap
+	 * Then call GetMember(id) to get the BankData.
+	 * if phone is null throw NullPointerException
+	 * if no bankData has given phone, throw NoSuchElementException
+	 * @return BankDataInterface
+	 * @param String phone
+	 */
 	@Override
 	public BankDataInterface GetMember(String phone) {
 		if(phone==null) throw new NullPointerException();
@@ -73,7 +95,14 @@ public class Backend implements BackendInterface{
 		}
 	}
 
-
+	/**
+	 * GetMember method which return the data of the member with given id.
+	 * Use the Given id to find the BankData in the Red Balck Tree.
+	 * if the id is null, throw NullPointerException
+	 * if the no member has given id, throw NoSuchElementException
+	 * @return BankDataInterface
+	 * @param Integer id
+	 */
 	@Override
 	public BankDataInterface GetMember(Integer id) {
 		if(id==null)throw new NullPointerException();
@@ -86,12 +115,21 @@ public class Backend implements BackendInterface{
 		
 	}
 	
-
+	/**
+	 * Insert method which insert a new Member into RedBlackTree and HashTableMap with the given value
+	 * The id of the new Member is size of RedBalckTee + 1.
+	 * throw IllegalArgumentException if the phone already exist
+	 * @param String name
+	 * @param Stirng sex
+	 * @param String phone
+	 * @param Integer age
+	 * @param Double balance
+	 */
 	@Override
-	public void Insert(String name, String sex, String phone, Integer age, Double balance) throws DataFormatException {
+	public void Insert(String name, String sex, String phone, Integer age, Double balance) {
 		if(map.containsKey(phone)) throw new IllegalArgumentException();
 		BankData newMember= new BankData();
-		int id = tree.size()+1;
+		int id = tree.size()+1000;
 		newMember.setID(id);
 		newMember.setAge(age);
 		newMember.setBalance(balance);
@@ -101,7 +139,15 @@ public class Backend implements BackendInterface{
 		tree.insert(newMember);
 		map.put(phone,id);
 	}
-
+	/**
+	 * The EditBalance method which edit the balance of the member with given id to the new Given Balance.
+	 * throw NullPointerException if id is null
+	 * throw NoSuchElementException if id does not exist
+	 * @param Integer id
+	 * @param Double balance
+	 * @throws NullPointerException
+	 * @throws NoSuchElementException
+	 */
 	@Override
 	public void EditBalance(Integer id, Double balance) {
 		try {
@@ -112,10 +158,11 @@ public class Backend implements BackendInterface{
 		}catch(NoSuchElementException e) {
 			throw new NoSuchElementException();
 		}
-		
-		
 	}
-
+	/**
+	 * The GetBlackList method return the List of BankData which has balance less than -1000
+	 * @return List<BankDataInterface>
+	 */
 	@Override
 	public List<BankDataInterface> GetBlacklist() {
 		List<BankDataInterface> BlackList = new ArrayList<BankDataInterface>();
@@ -125,5 +172,4 @@ public class Backend implements BackendInterface{
 		}
 		return BlackList;
 	}
-
 }
