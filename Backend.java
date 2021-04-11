1151f49251c8444c2ecf2ea67563e7cd9998039d
stevenYang914 <79009848+stevenYang914@users.noreply.github.com>
2021-04-11 17:01:11 +0800

    Update Backend.java

--- Backend.java
+++ Backend.java
@@ -1,3 +1,12 @@
+// --== CS400 File Header Information ==--
+// Name: Steven Yang
+// Email: yang558@wisc.edu
+// Team: AF Red
+// Role: Backend
+// TA: Mu
+// Lecturer: Gary
+// Notes to Grader: <optional extra notes>
+
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.Reader;
@@ -6,19 +15,32 @@ import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.zip.DataFormatException;
 
+/**
+ * The class Backend implements BackendInterface with several methods to modify the data.
+ * 
+ * @author Steven Yang
+ *
+ */
 public class Backend implements BackendInterface{
 
   List<CityDataInterface> cityList;
   CS400Graph<CityDataInterface> distanceGraph;
   CS400Graph<CityDataInterface> costGraph;
   
+  /**
+   * Create a graph with distance
+   * 
+   * @param cityList The city list
+   */
   private void createDistanceGraph(List<CityDataInterface> cityList) {
+    // insert all the city vertex
     for (int i = 0; i < cityList.size(); i++) {
       if (!distanceGraph.containsVertex(cityList.get(i))) {
         distanceGraph.insertVertex(cityList.get(i));
       }
     }
     
+    // insert all the edges by distance
     for (int i = 0; i < cityList.size(); i++) {
       CityDataInterface[] listOfConnected = cityList.get(i).GetListOfConneectedCity();
       int sizeOfConnectedArray = 0;
@@ -29,6 +51,7 @@ public class Backend implements BackendInterface{
           break;
         }
       }
+      
       int[] listOfDistance = cityList.get(i).GetListOfDistance();
       for (int j = 0; j < sizeOfConnectedArray; j++) {
         distanceGraph.insertEdge(cityList.get(i), listOfConnected[j], listOfDistance[j]);
@@ -36,13 +59,20 @@ public class Backend implements BackendInterface{
     }
   }
   
+  /**
+   * Create a graph with cost
+   * 
+   * @param cityList The city list
+   */
   private void createCostGraph(List<CityDataInterface> cityList) {
+    // insert all the city vertex
     for (int i = 0; i < cityList.size(); i++) {
       if (!costGraph.containsVertex(cityList.get(i))) {
         costGraph.insertVertex(cityList.get(i));
       }
     }
     
+    // insert all the edges by cost
     for (int i = 0; i < cityList.size(); i++) {
       CityDataInterface[] listOfConnected = cityList.get(i).GetListOfConneectedCity();
       int sizeOfConnectedArray = 0;
@@ -60,25 +90,39 @@ public class Backend implements BackendInterface{
     }
   }
   
+  /**
+   * This method is a constructor of backend class
+   * 
+   * @param CityInputFileReader city data
+   * @param PathInputFileReader path data
+   * @throws FileNotFoundException when file not found
+   * @throws IOException when IOExceptions
+   * @throws DataFormatException when DataFormatException
+   */
   public Backend(Reader CityInputFileReader, Reader PathInputFileReader)
       throws FileNotFoundException, IOException, DataFormatException {
-    try {
-      CityDataReader city = new CityDataReader();
-      this.cityList = city.readingCityDataSet(CityInputFileReader);
-      city.readingPathDataSet(PathInputFileReader, cityList);
-      distanceGraph = new CS400Graph<CityDataInterface>();
-      costGraph = new CS400Graph<CityDataInterface>();
-      createDistanceGraph(cityList);
-      createCostGraph(cityList);
-    } catch (FileNotFoundException e) {
-      throw new FileNotFoundException();
-    } catch (IOException e) {
-      throw new IOException();
-    }
+
+    CityDataReader city = new CityDataReader();
+    this.cityList = city.readingCityDataSet(CityInputFileReader);
+    city.readingPathDataSet(PathInputFileReader, cityList);
+    distanceGraph = new CS400Graph<CityDataInterface>();
+    costGraph = new CS400Graph<CityDataInterface>();
+    createDistanceGraph(cityList);
+    createCostGraph(cityList);
   }
   
+  /**
+   * This method returns the shortest Path between start and end
+   * 
+   * @param start start city
+   * @param end end city
+   * @return the shortest Path between start and end
+   * @throws NoSuchElementException when no path from start to end can be found
+   *         including when no vertex containing start or end can be found
+   */
   @Override
   public List<CityDataInterface> getShortestPath(String start, String end) {
+    // find vertex of start and end 
     CityDataInterface startV = null;
     CityDataInterface endV = null;
     
@@ -91,14 +135,28 @@ public class Backend implements BackendInterface{
       if (cityList.get(i).GetName().equals(end)) {
         endV = cityList.get(i);
       }
+    }
+    
+    if (startV == null || endV == null) {
+      throw new NoSuchElementException();
     }
     
     return distanceGraph.shortestPath(startV, endV);
     
   }
   
+  /**
+   * This method returns the shortest distance between start and end
+   * 
+   * @param start start city
+   * @param end end city
+   * @return the shortest distance between start and end
+   * @throws NoSuchElementException when no path from start to end can be found
+   *         including when no vertex containing start or end can be found
+   */
   @Override
   public int getShortestDistance(String start, String end) {
+    // find vertex of start and end 
     CityDataInterface startV = null;
     CityDataInterface endV = null;
     
@@ -111,13 +169,27 @@ public class Backend implements BackendInterface{
       if (cityList.get(i).GetName().equals(end)) {
         endV = cityList.get(i);
       }
+    }
+    
+    if (startV == null || endV == null) {
+      throw new NoSuchElementException();
     }
     
     return distanceGraph.getPathCost(startV, endV);
   }
 
+  /**
+   * This method returns the cheapest Path between start and end
+   * 
+   * @param start start city
+   * @param end end city
+   * @return the cheapest Path between start and end
+   * @throws NoSuchElementException when no path from start to end can be found
+   *         including when no vertex containing start or end can be found
+   */
   @Override
   public List<CityDataInterface> getCheapestPath(String start, String end) {
+    // find vertex of start and end 
     CityDataInterface startV = null;
     CityDataInterface endV = null;
     
@@ -130,13 +202,27 @@ public class Backend implements BackendInterface{
       if (cityList.get(i).GetName().equals(end)) {
         endV = cityList.get(i);
       }
+    }
+    
+    if (startV == null || endV == null) {
+      throw new NoSuchElementException();
     }
     
     return costGraph.shortestPath(startV, endV);
   }
 
+  /**
+   * This method returns the cheapest cost between start and end
+   * 
+   * @param start start city
+   * @param end end city
+   * @return the cheapest cost between start and end
+   * @throws NoSuchElementException when no path from start to end can be found
+   *         including when no vertex containing start or end can be found
+   */
   @Override
   public int getCheapestCost(String start, String end) {
+    // find vertex of start and end 
     CityDataInterface startV = null;
     CityDataInterface endV = null;
     
@@ -151,9 +237,20 @@ public class Backend implements BackendInterface{
       }
     }
     
+    if (startV == null || endV == null) {
+      throw new NoSuchElementException();
+    }
+    
     return costGraph.getPathCost(startV, endV);
   }
   
+  /**
+   * This method returns the list of all directly reachable cities of parameter city
+   * 
+   * @param city given city
+   * @return the list of all directly reachable cities of parameter city
+   * @throws NoSuchElementException when no vertex containing city can be found
+   */
   @Override
   public List<CityDataInterface> getDirectReach(String city) {
     List<CityDataInterface> directReach = new ArrayList<CityDataInterface>();
@@ -185,6 +282,16 @@ public class Backend implements BackendInterface{
     return directReach;
   }
 
+  /**
+   * This method add a path between source and target city
+   * 
+   * @param source start city
+   * @param target end city
+   * @param distance distance between start and end
+   * @param cost cost between start and end
+   * @return true if added successfully (no such path already exists), false otherwise
+   * @throws NoSuchElementException no vertex containing start or end can be found
+   */
   @Override
   public boolean addPath(String source, String target, int distance, int cost) {
     CityDataInterface startV = null;
@@ -208,6 +315,16 @@ public class Backend implements BackendInterface{
     return startV.AddPath(endV, distance, cost);
   }
 
+  /**
+   * This method delete a path between source and target city
+   * 
+   * @param source start city
+   * @param target end city
+   * @param distance distance between start and end
+   * @param cost cost between start and end
+   * @return true if deleted successfully (such path exists), false otherwise
+   * @throws NoSuchElementException no vertex containing start or end can be found
+   */
   @Override
   public boolean deletePath(String source, String target) {
     CityDataInterface startV = null;
@@ -230,9 +347,11 @@ public class Backend implements BackendInterface{
     
     List<CityDataInterface> connected = getDirectReach(source);
     int originalSize = connected.size();
+    int targetIndex = 0;
     for (int i = 0; i < connected.size(); i++) {
       if (connected.get(i).equals(endV)) {
         connected.remove(i);
+        targetIndex = i;
       }
     }
     
@@ -240,11 +359,66 @@ public class Backend implements BackendInterface{
       return false;
     }
     
+    // Delete Connected array
     CityDataInterface[] newArray = new CityDataInterface[100];
     for (int i = 0; i < connected.size(); i++) {
       newArray[i] = connected.get(i);
     }
     startV.SetListOfConnectedCity(newArray);
+    
+    // Delete Distance array
+    int sizeOfDistanceArray = 0;
+    int[] distance = startV.GetListOfDistance();
+    for (int h = 0; h < distance.length; h++) {
+      if (distance[h] != 0) {
+        sizeOfDistanceArray = sizeOfDistanceArray + 1;
+      } else {
+        break;
+      }
+    }
+    ArrayList<Integer> list = new ArrayList<Integer>();
+    for (int i = 0; i < sizeOfDistanceArray; i++) {
+      list.add(distance[i]);
+    }
+    
+    for (int i = 0; i < list.size(); i++) {
+      if (list.get(i) == distance[targetIndex]) {
+        list.remove(i);
+      }
+    }
+    
+    int[] newDistance = new int[100];
+    for(int i = 0; i < list.size(); i++) {
+      newDistance[i] = list.get(i);
+    }
+    startV.SetListOfDistance(newDistance);
+    
+    // Delete Cost array
+    int sizeOfCostArray = 0;
+    int[] cost = startV.GetListOfCost();
+    for (int h = 0; h < cost.length; h++) {
+      if (cost[h] != 0) {
+        sizeOfCostArray = sizeOfCostArray + 1;
+      } else {
+        break;
+      }
+    }
+    ArrayList<Integer> list2 = new ArrayList<Integer>();
+    for (int i = 0; i < sizeOfCostArray; i++) {
+      list2.add(cost[i]);
+    }
+    
+    for (int i = 0; i < list2.size(); i++) {
+      if (list2.get(i) == cost[targetIndex]) {
+        list2.remove(i);
+      }
+    }
+    
+    int[] newCost = new int[100];
+    for(int i = 0; i < list2.size(); i++) {
+      newCost[i] = list2.get(i);
+    }
+    startV.SetListOfCost(newCost);
     
     return true;
   }
