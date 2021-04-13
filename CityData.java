39ca211d84b87396db3aecd997e546a6d39ecb20
Cai92 <79020263+Cai92@users.noreply.github.com>
2021-04-13 11:19:57 +0800

    Update CityData.java

--- CityData.java
+++ CityData.java
@@ -1,4 +1,11 @@
-
+// --== CS400 File Header Information ==--
+// Name: Yuanqing Cai
+// Email: cai92@wisc.edu
+// Team: AF Red
+// Role: Data Wrangler
+// TA: Mu Cai
+// Lecturer: Florian
+// Notes to Grader: None
 public class CityData implements CityDataInterface{
   String name;
   int numberOfCitiesConnected;
@@ -9,22 +16,36 @@ public class CityData implements CityDataInterface{
     this.name = name;
     this.numberOfCitiesConnected = 0;
   }
-
+  /**
+   * get name of the city
+   * @return
+   */
   @Override
   public String GetName() {
     return this.name;
   }
-
+  /**
+   * get number of connected city
+   * @return
+   */
   @Override
   public int GetNumberOfCitiesConnected() {
     return this.numberOfCitiesConnected;
   }
-
+  /**
+   * get list of connected city
+   * @return
+   */
   @Override
   public CityDataInterface[] GetListOfConneectedCity() {
     return this.ListOfConnectedCity;
   }
-
+  /**
+   * get the distance of path from this city to target
+   * return 0 if no path between two city.
+   * @param target
+   * @return
+   */
   @Override
   public int GetDistance(CityDataInterface target) {
     for(int i=0;i<this.numberOfCitiesConnected;i++) {
@@ -33,7 +54,12 @@ public class CityData implements CityDataInterface{
     }
     return 0;
   }
-
+  /**
+   * get the cost of path from this city to target
+   * return 0 if no path between two city
+   * @param city
+   * @return
+   */
   @Override
   public int GetCost(CityDataInterface target) {
     for(int i=0;i<this.numberOfCitiesConnected;i++) {
@@ -43,17 +69,31 @@ public class CityData implements CityDataInterface{
     }
     return 0;
   }
-
+  /**
+   * get list of distance to connected city
+   * @return
+   */
   @Override
   public int[] GetListOfDistance() {
     return this.ListOfDistance;
   }
-
+  /**
+   * get list of cost to connected city
+   * @return
+   */
   @Override
   public int[] GetListOfCost() {
     return this.ListOfCost;
   }
-
+  /**
+   * add the path between this city and target.
+   * return true if there is no existed path between two cities and path added
+   * return false if there is already a path between two city.
+   * @param target
+   * @param distance
+   * @param cost
+   * @return
+   */
   @Override
   public boolean AddPath(CityDataInterface target, int distance, int cost) {
     for(int i=0;i<this.numberOfCitiesConnected;i++) {
@@ -66,7 +106,15 @@ public class CityData implements CityDataInterface{
     this.numberOfCitiesConnected++;
     return true;
   }
-
+  /**
+   * This method delete a path between source and target city
+   * 
+   * @param source start city
+   * @param target end city
+   * @param distance distance between start and end
+   * @param cost cost between start and end
+   * @return true if deleted successfully, false otherwise
+   */
   @Override
   public boolean DeletePath(CityDataInterface target) {
     for(int i=0;i<this.numberOfCitiesConnected;i++) {
@@ -82,12 +130,19 @@ public class CityData implements CityDataInterface{
     }
     return false;
   }
-
+  /**
+   * return true if the two city are the same
+   * @param other
+   * @return
+   */
   @Override
   public boolean equals(CityDataInterface other) {
     return this.GetName().equals(other.GetName());
   }
-
+  /**
+   * set the listofconnectedcity to given array
+   * @param array
+   */
   @Override
   public void SetListOfConnectedCity(CityDataInterface[] array) {
     this.ListOfConnectedCity = array;
